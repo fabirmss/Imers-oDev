1d58cb85254a9e8f3edd3637bc786e2b8f97204b
Fabi <108952843+fabirmss@users.noreply.github.com>
2022-09-05 23:29:28 -0400

    Add files via upload

---
+++ js.js
@@ -0,0 +1,13 @@
+var nome = "Fabiana"
+var primeiraNota = 9
+var segundaNota = 8
+var terceiraNota = 8
+var quartaNota = 7
+
+var notaFinal = (primeiraNota + segundaNota + terceiraNota + quartaNota)/4;
+
+
+
+console.log("Bem vindo!"+ nome);
+
+console.log(notaFinal);
